54653dbd7539ba933ee17154f780057a7ba69b49
Gareth Dean <gazdean@hotmail.com>
2024-03-06 18:24:26 +0000

    all content added

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Portfolio Page
+
+This is my first portfolio page after graduating from the Northcoders November2023 software engineering bootcamp
+
+Designed using “Progressive Advancement” "Mobile First" approach and using a single html page with navigation to allow for easy interaction.
+
+Created using vanilla html and css, with some javascript and DOM manipulation to allow me to easily add to or amend my projects section with any changes or new projects
---
+++ script.js
@@ -0,0 +1,94 @@
+const projectsData = [
+  {
+    title: "NC-News Backend Project",
+    description: "A backend Project using Postgres, Sql and render to go live",
+    image: "./Assets/Images/database.png",
+    github: "https://github.com/Gazdean/be-nc1-news1",
+    liveProject:"https://newsflash-e6p1.onrender.com",
+    borderColor:"red"
+  },
+  {
+    title: "NC-News Frontend Project",
+    description: "frontend react project which uses the NC-News backend project as an api",
+    image: "./Assets/Images/nc-news-frontend.png",
+    github: "https://github.com/Gazdean/fe-nc-news",
+    liveProject:""
+  },
+  {
+    title: "Flush Finder",
+    description: "frontend react toilet finding project whih allows you to use current location or a city drop down menu to find the toilets around you, and allows filtering by accessibilty, unisex and changing table to help those with particular needs",
+    image: "./Assets/Images/flush-finder.png",
+    githubLink: "https://github.com/OJ423/flush-finder",
+    liveProjectLink:"",
+    borderColor:"red"
+  },
+];
+
+const githubIcon = "fab fa-github"
+const webIcon = "fas fa-globe";
+
+function renderProjects() {
+
+  const projectsContainer = document.getElementById("projects-container");
+
+  projectsData.forEach((project) => {
+    const projectElement = document.createElement("div");
+    projectElement.className = "project";
+
+    console.log(projectElement)
+
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = project.title;
+    titleElement.className = "project-title";
+
+    const imageElement = document.createElement("img");
+    imageElement.src = project.image;
+    imageElement.alt = `a screen shot of the ${project.title} project`;
+    imageElement.className = "project-image";
+    imageElement.style.border = `2px solid ${project.borderColor}`;
+  
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = project.description;
+    descriptionElement.className = "project-description";
+
+    // Link Div anchor and icons
+
+    const iconContainer = document.createElement("div");
+    iconContainer.className = "icon-container";
+
+    const githubAnchorElement = document.createElement("a")
+    githubAnchorElement.href = project.github
+    githubAnchorElement.setAttribute("aria-label", `${project.title} GitHub Link`);
+
+    const liveProjectAnchorElement = document.createElement("a")
+    liveProjectAnchorElement.href = project.liveProject
+    liveProjectAnchorElement.setAttribute("aria-label", `${project.title} Live Project Link`);
+  
+
+    const githubIconElement = document.createElement("i")
+    githubIconElement.className = githubIcon
+
+    const liveProjectIconElement = document.createElement("i")
+    liveProjectIconElement.className = webIcon
+
+    githubAnchorElement.appendChild(githubIconElement)
+
+    liveProjectAnchorElement.appendChild(liveProjectIconElement)
+
+    iconContainer.appendChild(liveProjectAnchorElement)
+    iconContainer.appendChild(githubAnchorElement)
+
+    projectElement.appendChild(titleElement);
+    projectElement.appendChild(imageElement);
+    projectElement.appendChild(descriptionElement);
+    projectElement.appendChild(iconContainer);
+
+    projectsContainer.appendChild(projectElement);
+   
+    
+  });
+}
+
+// Call the function when the page is loaded
+document.addEventListener("DOMContentLoaded", renderProjects);
