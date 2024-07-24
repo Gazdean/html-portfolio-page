8f4d4c58cc7caafa42064d816ae997ba23b244e3
Gareth Dean <gazdean@hotmail.com>
2024-07-24 15:39:33 +0100

    added skills data

--- Assets/Data/skills-data.js
+++ Assets/Data/skills-data.js
@@ -1,8 +1,36 @@
 const skillsData = [
-    {
-      icon: "",
-      skillText:'',
-    }
-  ];
+  {
+    icon: "fa-brands fa-html5",
+    skillText: "HTML 5",
+  },
+  {
+    icon: "fa-brands fa-css3",
+    skillText: "CSS 3",
+  },
+  {
+    icon: "fa-brands fa-react",
+    skillText: "React",
+  },
+  {
+    icon: "fa-brands fa-react",
+    skillText: "React Native",
+  },
+  {
+    icon: "fa-brands fa-js",
+    skillText: "JavaScript",
+  },
+  {
+    icon: "fa-brands fa-bootstrap",
+    skillText: "HTML 5",
+  },
+  {
+    icon: "fa-brands fa-git",
+    skillText: "git",
+  },
+  {
+    icon: "fa-brands fa-github",
+    skillText: "github",
+  },
+];
 
-  export default skillsData;
+export default skillsData;
