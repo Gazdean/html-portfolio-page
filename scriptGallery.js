5741ab007629eb3c008194fd30c6c836967b852a
Gareth Dean <gazdean@hotmail.com>
2024-07-25 12:06:46 +0100

    create js logic to render gallery

---
+++ scriptGallery.js
@@ -0,0 +1,29 @@
+import galleryData from './Assets/Data/gallery-data.js'
+
+function renderGallery() {
+
+    const galleryItems = document.getElementById("gallery-items");
+  
+    galleryData.forEach((galleryItem) => {
+
+        const galleryItemElement = document.createElement("div");
+        galleryItemElement.className = "gallery-item";
+        
+        const imageElement = document.createElement("img");
+        imageElement.className = "gallery-image";
+        imageElement.src = galleryItem.imageSrc;
+        imageElement.alt = galleryItem.altText;
+        
+        galleryItemElement.appendChild(imageElement);
+
+        const titleElement = document.createElement("p");
+        titleElement.className = "gallery-image-title"
+        titleElement.innerText = galleryItem.title;
+
+        galleryItemElement.appendChild(titleElement);
+
+        galleryItems.appendChild(galleryItemElement);
+    });
+  }
+  // Call the function when the page is loaded
+  document.addEventListener("DOMContentLoaded", renderGallery());
