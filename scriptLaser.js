408d31a00b302928f3cb6c070c12a4e2bf155bdd
Gareth Dean <gazdean@hotmail.com>
2024-07-26 10:44:38 +0100

    delete laserTrail on screen resize

--- scriptLaser.js
+++ scriptLaser.js
@@ -1,4 +1,4 @@
-function createAndPositionDiv(id, x, y, width, height, colour) {
+function createAndPositionDiv(elementClass, x, y, width, height, colour) {
 
     const newDiv = document.createElement('div');
 
@@ -8,15 +8,14 @@ function createAndPositionDiv(id, x, y, width, height, colour) {
     newDiv.style.backgroundColor = colour; 
     newDiv.style.top = `${y}px`; 
     newDiv.style.left = `${x}px`;
-    newDiv.id = id;
+    newDiv.className = elementClass;
 
     const container = document.getElementById('home-card');
     container.appendChild(newDiv);
     let currentHeight = height;
     
-    if (newDiv.id === 'laserTrail') {
+    if (newDiv.className === 'laserTrail') {
         let screenWidth = window.innerWidth;
-        console.log(screenWidth) 
         let intervalTime = 8
         if (screenWidth < 450) intervalTime = 5
         function laserTrailHeightIncrease () {
@@ -43,13 +42,6 @@ function logGunPosition() {
         const relativeLeft = gunRect.left - homeCardRect.left;
         const relativeRight = gunRect.right - homeCardRect.left;
 
-        console.log('Position relative to home-card:');
-        console.log(`Top: ${relativeTop}px`);
-        console.log(`Left: ${relativeLeft}px`);
-        console.log(`Right: ${relativeRight}px`);
-        console.log(`Width: ${gunRect.width}px`);
-        console.log(`Height: ${gunRect.height}px`);
-
         createAndPositionDiv('laser', relativeLeft, relativeTop, 2, 5, 'red');
         createAndPositionDiv('laserTrail', relativeLeft, (relativeTop - 2), 3, 4, '#E8E8E8');
     } else {
@@ -58,13 +50,21 @@ function logGunPosition() {
 }
 
 function laserDivExists() {
-    console.log('hey')
     const div = document.getElementById('laser')
-    console.log('div', div)
     if (div === null) {logGunPosition()}
     else {
         laser.remove()
     }
 }
+
+
+window.addEventListener('resize', () => {
+    document.querySelectorAll('.laserTrail').forEach(element => {
+        element.remove()
+    });
+    document.querySelectorAll('.laser').forEach(element => {
+        element.remove()
+    });
+});
 
 setInterval(laserDivExists, 2300)
